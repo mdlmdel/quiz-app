8729b5a72adff3b82dc84cc3ef72bfbd09b7b45a
mdlmdel <mdlmdel@gmail.com>
2016-11-20 15:34:43 -0500

    Add index.html and app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document)ready(function () {
+  var quiz = [
+    {
+      // First question. Define as an object. 
+      // Properties: Question, possible answers, correct answer
+      question: "What is the stage name of Martin Karl Sandberg?",
+      answers: ["Andy Samberg", "Steve Sandberg", "Max Martin", "Carl Sandburg"],
+      correct: 2
+    }, 
+    // Second question
+    {
+      // Properties: Question, possible answers, correct answer
+      question: "What is the stage name of Martin Karl Sandberg?",
+      answers: ["Andy Samberg", "Steve Sandberg", "Max Martin", "Carl Sandburg"],
+      correct: 2
+    }]
+});
