2c5568919ca14cce29255128b5829ff035529f8a
MdL <mdlmdel@gmail.com>
2017-03-25 23:25:53 -0400

    Remove extraneous commas next to radio buttons, and add total possible to score display

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
 $(document).ready(function () {
+  // Below are the questions that will be included in the quiz app. 
+  // Properties of questions: Question, possible answers, correct answer.
   var quiz = [ 
-    // Below are the questions that will be included in the quiz app. 
-    // Properties of questions: Question, possible answers, correct answer.
-    // 1st question
+    
     {
       question: "What is the stage name of Martin Karl Sandberg?",
       answers: ["Andy Samberg", "Steve Sandberg", "Max Martin", "Carl Sandburg"],
       correct: 2
     }, 
-    // 2nd question
+    
     {
       question: "Who are the members of The Eurythmics?",
       answers: ["Annie Lennox and David A. Stewart", "Sting", "The Weeknd", "Grace Slick, Paul Kantner, Marty Balin, Jack Casady, Spencer Dryden"],
       correct: 0
     },
-    // 3rd question
+    
     {
       question: "Which of the following has a rumored synchronization effect between movie and album?",
       answers: ["Bon Jovi\'s Slippery When Wet and Titanic", "Pink Floyd\'s Dark Side of the Moon and The Wizard of Oz", "Sia\'s The Greatest and Ali", "AC/DC\'s The Razors Edge and Top Gun" ],
       correct: 1
     },
-    // 4th question
+    
     {
       question: "The musical Rent is loosely based on which opera?",
       answers: ["Turandot", "La Traviata", "Rigoletto", "La Boheme"],
       correct: 3
     },
-    // 5th question
+    
     {
       question: "Who sings Love on the Brain?",
       answers: ["Britney Spears", "Tina Turner", "Whitney Houston", "Rihanna"],
       correct: 3
     },
-    // 6th question
+    
     {
       question: "Which comedian sang Hallelujah by Leonard Cohen as a skit in response to his death and the presidential election results?",
       answers: ["Kate McKinnon", "Seth Myers", "Jimmy Fallon", "Chelsea Handler"],
       correct: 0
     },
-    // 7th question
+    
     {
       question: "Hugh Laurie, the star of the TV show House, plays which instrument in his band?",
       answers: ["Saxophone", "Clarinet", "Cello", "Piano"],
       correct: 3
     },
-    // 8th question
+    
     {
       question: "Bon Jovi\'s Livin\' on a Prayer is known for using this audio effect: ",
       answers: ["Telephone", "Autotune", "Talkbox", "Reverb"],
       correct: 2
     },
-    // 9th question
+    
     {
-      question: "The popular Shure SM58 microphone been shown to work in: ",
+      question: "The popular Shure SM58 microphone has been shown to work in: ",
       answers: ["Zero gravity", "Water", "Oil", "Mud"],
       correct: 0
     },
-    // 10th question
+    
     {
       question: "The well-known U87 vocal mic is produced by: ",
       answers: ["Shure", "Blue", "Neumann", "Sennheiser"],
@@ -118,11 +118,10 @@ $(document).ready(function () {
   // RENDER ANSWERS AS RADIO BUTTON ELEMENTS
   // Which question am I on? Look at the numbered question handed to us.
   var displayAnswers = function (num) {
-    var html = "";
     // Display the answers for this same question reference above.
     // We're running a function against every element of the answers array.
     // Index and map go together
-    html += quiz[num].answers.map(function(answer, index) {
+    var html = quiz[num].answers.map(function(answer, index) {
       return (
         '<li>' + 
           '<input class="choices" type="radio" name="radios" value="' + 
@@ -190,7 +189,7 @@ $(document).ready(function () {
       $('#results').show();
       $('.score').show();
       $('#reset').show();
-      $('#final-score').text('You got ' + score + ' questions correct.');
+      $('#final-score').text('You got ' + score + '/10 questions correct.');
     }
     else {
       displayQuestion(currentQuestion);
